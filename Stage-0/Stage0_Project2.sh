232a8ad33a8a80046d7e82854b2347b64bd8bbd0
afifahzahwah <zahwah.work@gmail.com>
2025-09-26 15:13:06 +0800

    Create Stage0_Project2.sh

---
+++ Stage-0/Stage0_Project2.sh
@@ -0,0 +1,45 @@
+Project 2: Installing Bioinformatics Software on the Terminal 
+# 1. Activate your base conda environment
+conda activate
+
+# 2. Create a conda environment named funtools
+conda create -n funtools
+
+# 3. Activate the funtools environment
+conda activate funtools
+
+# 4. Install Figlet using conda
+conda install -c conda-forge figlet
+
+# 5. Run figlet <your name>
+figlet AFIFAH_ZAHWAH
+
+# 6. Install bwa through the bioconda channel
+conda install -c bioconda bwa
+
+# 7. Install blast through the bioconda channel
+conda install -c bioconda blast
+
+# 8. Install samtools through the bioconda channel
+conda install -c bioconda samtools
+
+# 9. Install bedtools through the bioconda channel
+conda install -c bioconda bedtools
+
+# 10. Install spades.py through the bioconda channel
+conda install -c bioconda spades
+
+# 11. Install bcftools through the bioconda channel
+conda install -c bioconda bcftools
+
+# 12. Install fastp through the bioconda channel
+conda install -c bioconda fastp
+
+# 13. Install multiqc through the bioconda channel
+conda install -c bioconda multiqc# 1. Activate your base conda environment
+
+
+
+
+
+
