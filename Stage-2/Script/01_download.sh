2e13ce35ce9ad97c21cb650b555aff8f2f1e2f4e
afifahzahwah <zahwah.work@gmail.com>
2025-09-26 15:27:45 +0800

    Create 01_download.sh

---
+++ Stage-2/Script/01_download.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# 1) Download Samples
+# Script: 01_download.sh
+# Description: download control and treated sample according to lists
+echo "Downloading samples"
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/027/SRR12808527/SRR12808527.fastq.gz -o raw_fastq/ctl_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/028/SRR12808528/SRR12808528.fastq.gz -o raw_fastq/ctl_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/029/SRR12808529/SRR12808529.fastq.gz -o raw_fastq/ctl_3.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/099/SRR12808499/SRR12808499.fastq.gz -o raw_fastq/uvc_3.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/098/SRR12808498/SRR12808498.fastq.gz -o raw_fastq/uvc_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR128/097/SRR12808497/SRR12808497.fastq.gz -o raw_fastq/uvc_1.fastq.gz
